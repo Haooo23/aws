8c88a7160b9708dd3a4ea39cdebeabd92f9efc7f
Haooo23 <100354242+Haooo23@users.noreply.github.com>
2024-05-14 17:13:39 +0200

    Create login.php

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+include 'connect.php';
+
+session_start();    // avvio della sessione
+
+if (!isset($_POST['username']) || !isset($_POST['password'])) {
+    echo "vattene via";
+} else {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $password = md5($password);
+
+    // query per verificare se l'utente esiste gia nel database
+    $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        $_SESSION['logged'] = true;
+        $_SESSION['username'] = $username;
+        header("Location: ../pages/user.php");
+    } else {
+        $_SESSION['error_message'] = "Credenziali errate. Login fallito!";
+        header("Location: ../index.php"); 
+    }
+    $conn->close();
+}
+?>

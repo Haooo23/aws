0b4e03f468d369381459e028add9f6d891c8aabf
Haooo23 <100354242+Haooo23@users.noreply.github.com>
2024-05-14 18:15:22 +0200

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -8,7 +8,7 @@ $username = isset($_SESSION['username']) ? $_SESSION['username'] : 'Username';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Docker Project Setup</title>
+    <title>DOCKER PROJECT SETUP</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-5GXi7WbGMuZlTMbNdM1hiF+Qz3eXpbeuLz9XTv2T5MI5jldVP9hl0BfKVz4CEstB" crossorigin="anonymous">
     <style>
